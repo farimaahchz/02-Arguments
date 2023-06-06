c1d38b9210cf36b2de0cb464fb7378f63e2e1d1f
Farimah Chamanzadeh <121865417+farimaahchz@users.noreply.github.com>
2023-06-06 21:44:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
 
 # Add your code after this line
+
+#authorsnote: I would like to have the feedback in English.
+
 #Part 1: Greet Template
 def greet(name, greeting_template='Hello, <name>!'):
   return greeting_template.replace('<name>', name)
